63dd64aaef4cdf2558a4e784729e96d8860b04ac
lokeshyadav559 <31033117+lokeshyadav559@users.noreply.github.com>
2020-08-05 13:12:28 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-   
+    elif w.title() in data:
+        return data[w.title()]
     elif w.upper() in data:
         return data[w.upper()]
     elif len(get_close_matches(w,data.keys())) > 0:
